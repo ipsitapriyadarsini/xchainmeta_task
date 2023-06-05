0e83c032a0d764bcdbb9bcba20f07ccc3c00cec1
ipsitapriyadarsini <41966804+ipsitapriyadarsini@users.noreply.github.com>
2023-06-05 12:46:28 +0530

    Created the components,pages.Added react router and react icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage';
+import Settings from './pages/Settings.Jsx';
+import DataPage from './pages/DataPage';
+import SideBar from './components/SideBar';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <SideBar>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<DataPage />} />
+        <Route path="/" element={<Settings />} />
+      </Routes>
+    </SideBar>
   );
 }
 
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+const menuItems = [
+  {
+    path: "/",
+    title: "Home Page",
+    icon: ""
+  }, {
+    path: "/",
+    title: "Data Page",
+    icon: ""
+  }, {
+    path: "/",
+    title: "Settings",
+    icon: ""
+  }]
+
+const SideBar = () => {
+  return (
+    <nav className=''></nav>
+  )
+}
+
+export default SideBar
